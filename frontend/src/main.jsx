a188538b9c9da469749c37314e82561abc63f01b
Allegro <alex.makovka@gmail.com>
2025-06-20 19:22:20 +0300

    fix Top-level await

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,23 +9,48 @@ import { I18nextProvider, initReactI18next } from 'react-i18next';
 import App from './App.jsx';
 import resources from './locales/index.js';
 
-const i18n = i18next.createInstance();
+// const i18n = i18next.createInstance();
 
-await i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: 'ru',
-  });
+// await i18n
+//   .use(initReactI18next)
+//   .init({
+//     resources,
+//     fallbackLng: 'ru',
+//   });
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </I18nextProvider>
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <I18nextProvider i18n={i18n}>
+//       <Provider store={store}>
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//       </Provider>
+//     </I18nextProvider>
+//   </StrictMode>,
+// )
+
+const initApp = async () => {
+  const i18n = i18next.createInstance();
+
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: 'ru',
+    });
+
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <I18nextProvider i18n={i18n}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </I18nextProvider>
+    </StrictMode>
+  );
+};
+
+initApp();
